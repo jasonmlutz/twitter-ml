a17881911294525d9069fd4fd5bb94be65bd139e
Jason Lutz <jasonlutz@cilantro.local>
2019-02-27 15:20:11 -0800

    utilize template from Twitter's Single-user OAuth with Examples

--- twitter_ml.py
+++ twitter_ml.py
@@ -5,7 +5,7 @@ def oauth_req(url, http_method="GET", post_body="", http_headers=None):
     consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
     token = oauth2.Token(key=TOKEN_KEY, secret=TOKEN_SECRET)
     client = oauth2.Client(consumer, token)
-    resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
+    resp, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
     return content
 
 home_timeline = oauth_req("https://api.twitter.com/1.1/statuses/home_timeline.json")
