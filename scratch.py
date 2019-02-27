f5de363799eec1b5023eca61dacb6714d1c50c70
Jason Lutz <jasonlutz@cilantro.local>
2019-02-27 15:33:44 -0800

    moved initial sole twitter-ml.py function to scratch

--- scratch.py
+++ scratch.py
@@ -2,6 +2,18 @@ import oauth2 as oauth
 import json
 from credentials import *
 
+#def oauth_req(url, http_method="GET", post_body="", http_headers=None):
+#    """ From https://developer.twitter.com/en/docs/basics/authentication/guides/single-user
+#    """
+#    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+#    token = oauth2.Token(key=TOKEN_KEY, secret=TOKEN_SECRET)
+#    client = oauth2.Client(consumer, token)
+#    resp, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
+#    return content
+#
+#home_timeline = oauth_req("https://api.twitter.com/1.1/statuses/home_timeline.json")
+
+
 consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
 access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
 client = oauth.Client(consumer, access_token)
--- twitter_ml.py
+++ twitter_ml.py
@@ -1,11 +1,6 @@
-import oauth2
+import oauth2 as oauth
 from credentials import *
 
-def oauth_req(url, http_method="GET", post_body="", http_headers=None):
-    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-    token = oauth2.Token(key=TOKEN_KEY, secret=TOKEN_SECRET)
-    client = oauth2.Client(consumer, token)
-    resp, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
-    return content
-
-home_timeline = oauth_req("https://api.twitter.com/1.1/statuses/home_timeline.json")
+def oauth_twitter_search(query, lang="en", result_type = "recent", count = 100, ):
+    """ Search Twitter ...
+    """
