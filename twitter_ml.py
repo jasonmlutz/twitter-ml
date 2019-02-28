e2dc110f471cdc0c93cc48b74d8aae2478eab668
Jason Lutz <jasonlutz@cilantro.local>
2019-02-27 22:14:23 -0800

    wrrite and test first draft of oauth_twitter_search

--- twitter_ml.py
+++ twitter_ml.py
@@ -1,6 +1,17 @@
 import oauth2 as oauth
+import json
 from credentials import *
 
-def oauth_twitter_search(query, lang="en", result_type = "recent", count = 100, ):
+def oauth_twitter_search(query, consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, lang="en", result_type="recent", count=100):
     """ Search Twitter ...
+    looks like Tweets with "truncated": true could pose a problem
     """
+    search_endpoint = "https://api.twitter.com/1.1/search/tweets.json"
+    compiled_search_endpoint = "{}?q={}&count={}&result_type={}&lang={}".format(search_endpoint, query, count, result_type, lang)
+    consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+    client = oauth.Client(consumer)
+    response, data = client.request(compiled_search_endpoint)
+    tweets = json.loads(data)
+    return tweets
+
+a = oauth_twitter_search("nasa")
